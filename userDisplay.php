36b145f2afd4eafec18a04f8baba3f03cbbb036f
otatakahiro <ota7281@gmail.com>
2022-08-28 19:00:46 -0700

    Display function

--- userDisplay.php
+++ userDisplay.php
@@ -1,76 +1,113 @@
-<?php
-    include './config.php';
-
-    $addr= './files/DB-user.json';
-    $fileHandler = fopen($addr, 'r');
-    $data = fread($fileHandler, filesize($addr));
-    fclose($fileHandler);
-
-    $userDatas = json_decode($data);
-
+<?php 
+    include './config.php'; 
     $dbCon = new mysqli($dbServername, $dbUsername, $dbPass, $dbName);
-    $selectCmd = "SELECT * FROM user_tb;";
-    $result = $dbCon->query($selectCmd);
-
-    if($result->num_rows == 0){
-        foreach($userDatas as $userData){
-        $createDate = date("Y-m-d",strtotime($userData->create_id_date));
-        $birthdayDate = date("Y-m-d",strtotime($userData->birthday));
-        $pass = password_hash($userData->password, PASSWORD_BCRYPT, ["cost"=>9]);
-
-        $insertCmd = "INSERT INTO user_tb (user_id, password, email, create_id_date, gender, birthday, login_failure_num) VALUES ('$userData->user_id', '$pass', '$userData->email', '$createDate', '$userData->gender', '$birthdayDate', '$userData->login_failure_num')";
-
-        if($dbCon->query($insertCmd) === true){
-            echo "done";
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        if(isset($_POST["keyword"]) && (strlen($_POST["keyword"]) > 2)){
+            $keyword = $_POST["keyword"];
+            if($dbCon->connect_error){
+                die("Connect error");
+            }else{
+                $resultArray = [];
+                $searchPostCmd = "SELECT * FROM post_tb WHERE tags LIKE '%$keyword%'";
+                $result = $dbCon->query($searchPostCmd);
+                while($row = $result->fetch_assoc()){
+                array_push($resultArray, $row);
+                }
+            }
         }else{
-            echo "false".$dbCon->error;
+            echo "Keyword is too short";
         }
-        echo "</br>";
+    }else{
+        if($dbCon->connect_error){
+            die("Connect error");
+        }else{
+            $resultArray = [];
+            $searchPostCmd = "SELECT * FROM post_tb";
+            $result = $dbCon->query($searchPostCmd);
+            while($row = $result->fetch_assoc()){
+                array_push($resultArray, $row);
+            }
         }
     }
-    $dbCon->close();
+
+    
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/search.css">
 </head>
 <body>
-    <table>
-        <thead>
-            <tr>
-                <th>User ID</th>
-                <th>Password</th>
-                <th>Email</th>
-                <th>Create Account</th>
-                <th>Gender</th>
-                <th>Birthday</th>
-                <th>Last Login</th>
-            </tr>
-        </thead>
-    <?php
-        function loadData($userData){
-            foreach($userData as $idx=>$user){
-                echo "<tr><td>$user->user_id</td><td>$user->password</td><td>$user->email</td><td>$user->create_id_date</td><td>$user->gender</td><td>$user->birthday</td><td>$user->login_failure_num</td>";
-                echo "<td> <form method='POST' action='";
-                echo $_SERVER['PHP_SELF']."'><a href='./userEdit.php?idx=$idx'>Edit</a></td>";
-                echo "<td><a href='./deleteUser.php?idx=$idx'>Delete</a></td></tr>";
-            }
-            echo "</table>";
+  <h1>PURL DASHBOARD</h1>
+  <form method="POST" action="<?php $_SERVER['PHP_SELF'];?>">
+  <?php
+    echo "<input type='text' name='keyword'/>";
+    echo "<button type='submit'>Search</button>";
+  ?>
+  </form>
+  <?php
+    $dbcon = new mysqli($dbServername,$dbUsername,$dbPass,$dbName);
+    if ($dbcon->connect_error){
+        die("Connection error");
+    }else{
+        if(isset($resultArray)){
+        $path = ".";
+        echo "<table><thead><tr><th>User ID</th><th>Post UID</th><th>Post Date</th><th>Photo</th><th>Tags</th><th>Address</th><th colspan='3'>Actions</th></tr></thead>";
+        echo "<tbody>";
+        foreach($resultArray as $key=>$post){
+            $index = "post".$key;
+            echo "<tr><td>".$post["user_id"]."</td>";
+            echo "<td>".$post["post_uid"]."</td>";
+            echo "<td>".$post["post_date"]."</td>";
+            echo "<td><img style='width:100%;' src=".$path.$post["photo_src"]." alt=".$post["tags"]."><a href='".$path.$post["photo_src"]."' download>Download</a>"."</td>";
+            echo "<td>".$post["tags"]."</td><td><span>".$post["addr"]."</span></br><button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#".$index."'>Open the map</button>"."</td></tr>";
+           
         }
-        $filehandler = fopen('./files/DB-user.json','r');
-        $data = fread($filehandler,filesize('./files/DB-user.json'));
-        fclose($filehandler);
-        $userData = json_decode($data);
-        if($_SERVER['REQUEST_METHOD']=="GET"){
-            loadData($userData);
+        echo "</tbody></table>";
+
         }
-    ?>
-    </table>
+        $dbcon->close();
+    }
+  ?>
+  <?php
+    if(isset($resultArray)){
+      foreach($resultArray as $key=>$post){
+        $index = "post".$key;
+        $addr = str_replace(" ", "+", $post["addr"]);
+  ?>
+  <div class="modal fade" id="<?php echo $index;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Post date : <?php echo $post["post_date"];?></h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        <iframe
+          width="450"
+          height="250"
+          frameborder="0" style="border:0"
+          referrerpolicy="no-referrer-when-downgrade"
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBunR-4sODBjn5hcvBJmzf9L7_pKF905R4&q=<?php echo $addr;?>"
+          allowfullscreen>
+        </iframe>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+      }
+    }
+  ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
